886b3c59ced9d7cb63b4b11562cd77e4c9496518
Rick Knegt <rknegt180@gmail.com>
2023-02-10 19:39:59 +0100

    Add UID and LOGINUID to the columns as output and made showing environment variables optional

--- psall.py
+++ psall.py
@@ -1,10 +1,13 @@
 from typing import Optional
 
-from volatility3.framework import exceptions, interfaces, renderers
+from volatility3.framework import exceptions, interfaces, renderers, constants
 from volatility3.framework.configuration import requirements
 from volatility3.framework.interfaces import plugins
 from volatility3.framework.objects import utility
 from volatility3.plugins.linux import pslist, psaux
+
+
+KERNEL_UID32_T_SIZE = 2
 
 
 class PsAll(plugins.PluginInterface):
@@ -30,7 +33,13 @@ class PsAll(plugins.PluginInterface):
             ),
             requirements.BooleanRequirement(
                 name="userOnly",
-                description="Only show the user processes ",
+                description="Only show the user processes",
+                default=False,
+                optional=True,
+            ),
+            requirements.BooleanRequirement(
+                name="showEnv",
+                description="Show environment variables",
                 default=False,
                 optional=True,
             ),
@@ -70,13 +79,16 @@ class PsAll(plugins.PluginInterface):
 
         return env
 
-    def _generator(self, tasks, userOnly: bool):
+    def _generator(self, tasks, userOnly: bool, showEnv: bool):
         """Yield all the tasks with their relevant information
 
         Arguments:
         tasks -- list of processes
         userOnly -- only show user processes
         """
+        vm_linux = self.context.modules[self.config["kernel"]]
+        default_symbol = vm_linux.symbol_table_name + constants.BANG
+
         for task in tasks:
             pid = task.pid
 
@@ -89,45 +101,69 @@ class PsAll(plugins.PluginInterface):
 
             env = self._get_command_line_env(task)
 
-            if env:
-                logname = env.get("LOGNAME")
+            """ Create the cred object because it is necessary for reading the UID. """
+            cred = self.context.object(
+                default_symbol + "cred",
+                offset=task.cred,
+                layer_name=vm_linux.layer_name,
+            )
 
-                env_str = "\n----------\nENV:\n"
-                for key, val in env.items():
-                    env_str += f"- {key}={val}\n"
-            else:
-                if userOnly:
+            """Read the UID from the task_struct in bytes because we can't access it through objects. """
+            uid = int.from_bytes(
+                self._context.layers[vm_linux.layer_name].read(
+                    cred.uid.vol.offset, KERNEL_UID32_T_SIZE
+                ),
+                "little",
+            )
+
+            """Read the LOGINUID from the task_struct in bytes because we can't access it through objects. """
+            loginuid = int.from_bytes(
+                self._context.layers[vm_linux.layer_name].read(
+                    task.loginuid.vol.offset, KERNEL_UID32_T_SIZE
+                ),
+                "little",
+            )
+
+            """ Only yield environment variables if the showEnv is True.
+            Skip all kernel threads (kernel threads don't have environment variables),
+            if userOnly is True. """
+            if showEnv:
+                if env:
+                    env_str = "\n----------\nENV:\n"
+                    for key, val in env.items():
+                        env_str += f"- {key}={val}\n"
+                elif userOnly:
                     continue
                 else:
-                    logname = ""
                     env_str = ""
-
-            yield (
-                0,
-                (
-                    f"{pid=}",
-                    f"{ppid=}",
-                    f"{logname=}",
-                    f"{name=}",
-                    f"\n{args=}",
-                    env_str,
-                ),
-            )
+                yield (0, (pid, ppid, loginuid, uid, name, args, env_str))
+            else:
+                if not env and userOnly:
+                    continue
+                else:
+                    yield (0, (pid, ppid, loginuid, uid, name, args))
 
     def run(self):
         filterLogname = self.config.get("filterLogname")
+        showEnv = self.config.get("showEnv")
+
+        columns = [
+            ("PID", int),
+            ("PPID", int),
+            ("LOGINUID", int),
+            ("UID", int),
+            ("COMM", str),
+            ("ARGS", str),
+        ]
+
+        if showEnv:
+            columns.append(("ENV", str))
 
         return renderers.TreeGrid(
-            [
-                ("PID", str),
-                ("PPID", str),
-                ("LOGNAME", str),
-                ("COMM", str),
-                ("ARGS", str),
-                ("ENV", str),
-            ],
+            columns,
             self._generator(
                 pslist.PsList.list_tasks(self.context, self.config["kernel"]),
                 userOnly=filterLogname,
+                showEnv=showEnv,
             ),
         )
